cca09fa8a7a17524a383adbb0cf94c77888fff4f
benmartin688 <benmartin688@yahoo.co.nz>
2023-12-30 12:15:35 +1300

    Removed placeholder code

--- src/cards.js
+++ src/cards.js
@@ -3,14 +3,4 @@ import two from "./assets/cards/2.png";
 import three from "./assets/cards/3.png";
 import four from "./assets/cards/4.png";
 
-export const cards = [
-  one,
-  two,
-  three,
-  four,
-  // "https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg",
-  // "https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg",
-  // "https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg",
-  // "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg",
-  // "https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg",
-];
+export const cards = [one, two, three, four];
